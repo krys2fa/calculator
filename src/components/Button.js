a798a90a656269a87f2a39139ec5df451bb43502
Christopher Amanor <kriskit7@gmail.com>
2020-09-18 10:06:16 +0000

    Implement the Button component with name prop

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ name }) => (
+  <div id="button">
+    { name }
+  </div>
+);
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+};
