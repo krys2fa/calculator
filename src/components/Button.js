44c57794ebc28574f54f79e7bba15c4944102f5d
Christopher Amanor <kriskit7@gmail.com>
2020-09-19 00:38:53 +0000

    Add styles to App, Display, Button and ButtonPanel components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => (
-  <div id="button">
-    { name }
+const Button = ({ name, wide, color }) => (
+  <div
+    className="button"
+    style={{
+      backgroundColor: color ? '#e0e0e0' : '',
+      width: wide ? '50%' : '',
+    }}
+  >
+    {name}
   </div>
 );
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.bool,
+  wide: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: false,
+  wide: false,
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,34 +2,34 @@ import React from 'react';
 import Button from './Button';
 
 const ButtonPanel = () => (
-  <div id="panel">
+  <div id="panel" className="button-panel">
     <div id="group-1">
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
+      <Button name="AC" color />
+      <Button name="+/-" color />
+      <Button name="%" color />
       <Button name="÷" />
     </div>
     <div id="group-2">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
+      <Button name="7" color />
+      <Button name="8" color />
+      <Button name="9" color />
       <Button name="X" />
     </div>
     <div id="group-3">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
+      <Button name="4" color />
+      <Button name="5" color />
+      <Button name="6" color />
       <Button name="-" />
     </div>
     <div id="group-4">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
+      <Button name="1" color />
+      <Button name="2" color />
+      <Button name="3" color />
       <Button name="+" />
     </div>
     <div id="group-5">
-      <Button name="0" />
-      <Button name="." />
+      <Button name="0" wide color />
+      <Button name="." color />
       <Button name="=" />
     </div>
   </div>
