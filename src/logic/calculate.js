3b08f2965a904586e61880ef12eb4ff3d5b76dee
Christopher Amanor <kriskit7@gmail.com>
2020-09-21 20:58:58 +0000

    Handle all buttonName cases in calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,22 +1,34 @@
+/* eslint-disable radix */
 import operate from './operate';
 
 const calculate = (data, buttonName) => {
-  const { total, next, operation } = data;
+  let { total, next, operation } = data;
 
   switch (buttonName) {
-    case '+/-':
-      return operate(total, next, operation);
-
-    case '+':
-      return operate(total, next, operation);
-
-    case '-':
-      return operate(total, next, operation);
-
-    case '÷':
-      return operate(total, next, operation);
-
     case '%':
+    case '÷':
+    case '-':
+    case '+':
+    case '+/-':
+      operation = buttonName;
+      return { total, next, operation };
+    case 'AC':
+      return { total: '0', next: null, operation: null };
+    case '.':
+      if ((total && !next) || (!total && !next)) {
+        total += buttonName;
+      } else if (operation) {
+        next += buttonName;
+      }
+      return { total, next, operation };
+    case [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(parseInt(buttonName)):
+      if (operation) {
+        next += buttonName;
+      } else if (!operation) {
+        total += buttonName;
+      }
+      return { total, next, operation };
+    case '=':
       return operate(total, next, operation);
 
     default:
