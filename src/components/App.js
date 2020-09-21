39abe6ff5f943293856dfdbda5e2744135c93b8f
Christopher Amanor <kriskit7@gmail.com>
2020-09-21 15:08:06 +0000

    Implement render and handleClick functions for the App component

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,29 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <div id="app" className="app">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    this.setState(prevState => calculate(prevState, buttonName));
+  }
+
+  render() {
+    return (
+      <div id="app" className="app">
+        <Display />
+        <ButtonPanel onClick={this.handleClick} />
+      </div>
+    );
+  }
+}
 
 export default App;
