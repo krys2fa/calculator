5e52808926f3951191976d06f1528696f6740144
Christopher Amanor <kriskit7@gmail.com>
2020-09-23 01:23:35 +0000

    Fix division by zero error

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,12 +1,6 @@
 import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
-  // Big.DP = 40;
-  // console.log('operate -> numberOne', numberOne);
-  // let total = new Big(parseInt(numberOne, 10));
-  // console.log('operate -> total11', total.toString());
-  // const next = new Big(parseInt(numberTwo, 10));
-
   let total = new Big(numberOne);
   const next = new Big(numberTwo);
 
@@ -21,7 +15,7 @@ const operate = (numberOne, numberTwo, operation) => {
       total = total.times(next).toPrecision();
       break;
     case '÷':
-      total = total.div(next).toPrecision();
+      total = numberTwo === '0' ? '0' : total.div(next).toPrecision();
       break;
     case '%':
       total = total.times(0.01).toPrecision();
